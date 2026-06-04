b1fe579dc958f433826da0a60fb8671d22da1c04
Gomnam320 <gomnam320@gmail.com>
2026-06-04 18:09:13 +0330

    update setup.py: add findBeginOfWeek & logBuilder

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 import time
 import random
 
-def diffBuilder():
+dis=[2,4,7,13,15,16,17,18,19]
+oneDay=8400
+
+commitCommand="git commit -m '#' --date '$'"
+
+def logBuilder(save = False):
+    with open("log.md", "a") as log:
+        log.write("\nThis is a simple edit for **smile Make** :)\n")
+
+def findBeginOfWeek(start=0,now=time.time()):
+    return now - (now-3*oneDay) % (7*oneDay) + start * oneDay + 1 # + 1 is to certainly
+
+def start():
     pass
+
+
